af94c8dda32b7c6c15cf94949a7b1c33be5a9755
bav735 <bav735@mail.ru>
2017-11-07 15:16:19 +0300

    xml

--- src/GadgetGroup.java
+++ src/GadgetGroup.java
@@ -11,7 +11,7 @@ public class GadgetGroup extends Gadgets {
     final static int HOUR_BEGIN = 9;
     final static int ADS_COUNT_BORDER = 300;
     final static int DAYS_OFFSET = 2;
-    final static int HOUR_OFFSET = 5;
+    final static int HOUR_OFFSET = 7;
     final static int MINUTE_OFFSET = 0;
     ArrayList<ArrayList<String>> gadgets;
     String country;
@@ -180,10 +180,11 @@ public class GadgetGroup extends Gadgets {
         } else {
             text += getGadgetName(gadgets.get(0), getFirstAttr(), COLOR) + "<br>";
             String warrantyCost = getPrice(gadgets.get(0), YEAR_WARRANTY_COST);
-            if (warrantyCost.length() > 0) {
-                text += "гарантия на 1 год +" + warrantyCost + "\u20BD к цене<br>";
+            if (warrantyCost.length() > 1) {
+                text += "гарантия на 1 год +" + warrantyCost + "\u20BD<br>";
             }
-            text += "-запечатан в пленку, полный комплект; " + GadgetConst.MAP_QUALITY_DESCRIPTION.get(getQuality(gadgets.get(0)));
+            text += "цена с частичной предоплатой = " + getPriceByCity(gadgets.get(0), cityId - 1) + "\u20BD<br>";
+            text += "</p><p>-запечатан в пленку, полный комплект; " + GadgetConst.MAP_QUALITY_DESCRIPTION.get(getQuality(gadgets.get(0)));
             text += ", версия/прошивка " + country + ";";
             if (getQuality(gadgets.get(0)).startsWith(GadgetConst.REF)) {
                 text += " в наличии также имеется новая ";
@@ -193,7 +194,7 @@ public class GadgetGroup extends Gadgets {
                 text += "продукция с гарантией производителя,";
             }
             text += " характеристики и ассортимент см. на официальном сайте iSPARK";
-            text += "</p><p>Кредит/Рассрочка. Трейд-ин. Оплата по карте без %<br>Самовывоз" +
+            text += "</p><p>Кредит/Рассрочка. Трейд-ин. Оплата по карте<br>Самовывоз" +
                     " Москва/Казань/Чебоксары. Доставка по РФ";
         }
         text += "</p>]]>";
